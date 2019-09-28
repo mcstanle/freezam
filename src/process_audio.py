e3414648222649c109c7000372fd8bd437f514f3
Mike Stanley <mcstanle@andrew.cmu.edu>
2019-09-28 14:02:47 -0400

    began writing process_audio class. Added a directory for notebooks for data and method exploration.

--- src/process_audio.py
+++ src/process_audio.py
@@ -4,9 +4,59 @@ Broadly, the code in this file should do the following:
 1. Apply a window function to an audio file.
 2. Compute local periodograms from windowed signal.
 3. Smooth Periodograms.
+4. Compute window signature.
 
 ===============================================================================
+
 Author:        Mike Stanley
 Created:       September 28, 2019
 Last Modified: September 28, 2019
+
+===============================================================================
+
+TODO:
+1. How do we deal with the left and right signal?
+2. Understand the output of the create_windows function.
+3. finish the periodogram function
+4. start the signatures function
+
 '''
+
+class process_audio:
+    """
+    Class to configure audio processing pipeline. This pipeline should include 
+    functions that:
+    - produce a windowed time series
+    - compute periodogram of windowed time series
+    - compute signatures of windowed time series
+
+    Note, this class doesn't have any read capabilities because that taks is 
+    handled by the database class.
+    """
+    def __init__(self, window_type, window_size, window_shift):
+        self.window_type=window_type
+        self.window_size=window_size
+        self.window_shift=window_shift
+
+
+    def create_windows(self, data, fs):
+        """
+        Created a windowed time series from data. 
+
+        Parameters:
+            data (numpy array) : 2 column numpy array with L and R signals
+            fs (int) : frequency sample
+
+        Returns:
+            ????
+        """
+        pass
+
+    def create_periodogram(self, single_window):
+        """
+        Given a single window of a time series, computes the periodogram.
+
+        Parameters:
+            single_window (numpy array) : 
+        """
+        pass
--- tests/test_process_audio.py
+++ tests/test_process_audio.py
@@ -8,3 +8,5 @@ Last Modified: September 28, 2019
 '''
 
 import pytest
+
+from process_audio import process_audio
